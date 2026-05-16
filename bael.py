a73c791f010a82d72c323903772e2192307fc418
Jackal <thatonestupidwhiteboi@gmail.com>
2026-05-16 12:11:12 -0600

    Patched __version__

--- bael.py
+++ bael.py
@@ -50,7 +50,7 @@ except ImportError:
     CRYPTO_AVAILABLE = False
     class InvalidTag(Exception): pass
 
-__version__ = "0.2.3"
+__version__ = "0.2.4"
 __author__ = "J4ck3LSyN"
 
 # ====================== LOGGING ======================
